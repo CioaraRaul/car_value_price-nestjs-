f30dbab0fe461421f2b7efdafa303c0c5fe09f8c
CioaraRaul <cioararaul08@gmail.com>
2025-12-27 15:36:08 +0200

    relations with typeorm

--- src/users/interceptors/current-user.interceptor.ts
+++ src/users/interceptors/current-user.interceptor.ts
@@ -9,14 +9,13 @@ import { UsersService } from '../users.service';
 
 @Injectable()
 export class CurrentUserInterceptor implements NestInterceptor {
-  constructor(private usersService: UsersService) {}
+  constructor(private userService: UsersService) {}
 
   async intercept(context: ExecutionContext, handler: CallHandler) {
     const request = context.switchToHttp().getRequest();
     const { userId } = request.session || {};
-
     if (userId) {
-      const user = await this.usersService.findOne(userId);
+      const user = await this.userService.findOne(userId);
       request.currentUser = user;
     }
 
